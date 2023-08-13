a1ce5d113470c94caa13e492c200f74f57689ee8
Stephen Gutekanst <stephen@hexops.com>
2023-08-13 10:46:32 -0700

    run through the getting-started guide
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# mach-core 'getting started' project
+
+We walked through the [mach-core getting started guide](https://machengine.org/core/getting-started) and committed the result to this repository.
+
+To run it, see [these instructions](https://machengine.org/next/core/getting-started/#building-your-project)
+
+Feel free to use this start project as you see fit!
---
+++ build.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const mach_core = @import("mach_core");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) !void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    mach_core.mach_glfw_import_path = "mach_core.mach_gpu.mach_gpu_dawn.mach_glfw";
+    const app = try mach_core.App.init(b, .{
+        .name = "myapp",
+        .src = "src/main.zig",
+        .target = target,
+        .optimize = optimize,
+        .deps = &[_]std.build.ModuleDependency{},
+    });
+    if (b.args) |args| app.run.addArgs(args);
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&app.run.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
